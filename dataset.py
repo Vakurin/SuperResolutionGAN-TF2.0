6fda76dc7fbe1566ea376fd3c1d587ee3f7165d1
Vakurin <vakurin.maxim@gmail.com>
2020-03-07 13:43:09 +0700

    Add Lib Imports

--- dataset.py
+++ dataset.py
@@ -1,3 +1,6 @@
+import tensorlayer as tl
+import tensorflow as tf
+
 def get_train_data(img_hr_size, img_lr_size, batch_size, path_folder_with_images, shuffle_buffer_size=128):
 
     # List file names
